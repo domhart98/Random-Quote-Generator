98b7f05ffd33bcae9a640cfc8d400f1a2c258526
domhart98 <domhart98@hotmail.com>
2023-03-28 16:26:39 -0400

    Add files via upload

--- src/drumMachine.js
+++ src/drumMachine.js
@@ -1,57 +1,57 @@
-import { useEffect, useState } from 'react';
+import { useCallback, useEffect, useState } from 'react';
 import './drumMachine.css'
 
-
+const sounds = [
+    {keyCode: 81,
+        key: 'Q',
+        id: 'drum1',
+        url: './src_sounds_hihat.wav'
+    },
+    {keyCode: 87,
+        key: 'W',
+        id: 'drum2',
+        url: 'LeBlnche - Gangsta Snare.wav'
+    },
+    {keyCode: 69,
+        key: 'E',
+        id: 'drum3',
+        url: 'LeBlnche - Clippers Hi-Hat.wav'
+    },
+    {keyCode: 65,
+        key: 'A',
+        id: 'drum4',
+        url: 'LeBlnche - Compton Rim.wav'
+    },
+    {keyCode: 83,
+        key: 'S',
+        id: 'drum5',
+        url: 'LeBlnche - Tambourine Perc 1.wav'
+    },
+    {keyCode: 68,
+        key: 'D',
+        id: 'drum6',
+        url: 'LeBlnche - Heavy Hat Perc.wav'
+    },
+    {keyCode: 90,
+        key: 'Z',
+        id: 'drum7',
+        url: 'LeBlnche - Royale Kick.wav'
+    },
+    {keyCode: 88,
+        key: 'X',
+        id: 'drum8',
+        url: 'LeBlnche - tradition Perc.wav'
+    },
+    {keyCode: 67,
+        key: 'C',
+        id: 'drum9',
+        url: 'LeBlnche - Miami Rim.wav'
+    }
+]
 const DrumMachine = () =>{
     const[display, setDisplay] = useState("")
-    const sounds = [
-        {keyCode: 81,
-            key: 'Q',
-            id: 'drum1',
-            url: './src_sounds_hihat.wav'
-        },
-        {keyCode: 87,
-            key: 'W',
-            id: 'drum2',
-            url: 'LeBlnche - Gangsta Snare.wav'
-        },
-        {keyCode: 69,
-            key: 'E',
-            id: 'drum3',
-            url: 'LeBlnche - Clippers Hi-Hat.wav'
-        },
-        {keyCode: 65,
-            key: 'A',
-            id: 'drum4',
-            url: 'LeBlnche - Compton Rim.wav'
-        },
-        {keyCode: 83,
-            key: 'S',
-            id: 'drum5',
-            url: 'LeBlnche - Tambourine Perc 1.wav'
-        },
-        {keyCode: 68,
-            key: 'D',
-            id: 'drum6',
-            url: 'LeBlnche - Heavy Hat Perc.wav'
-        },
-        {keyCode: 90,
-            key: 'Z',
-            id: 'drum7',
-            url: 'LeBlnche - Royale Kick.wav'
-        },
-        {keyCode: 88,
-            key: 'X',
-            id: 'drum8',
-            url: 'LeBlnche - tradition Perc.wav'
-        },
-        {keyCode: 67,
-            key: 'C',
-            id: 'drum9',
-            url: 'LeBlnche - Miami Rim.wav'
-        }
-    ]
-    const handleKeyDown = (e) =>{
+    
+    const handleKeyDown = useCallback((e) =>{
 
         if(e.keyCode === 81){
             playSound(sounds[0].key)
@@ -80,7 +80,7 @@ const DrumMachine = () =>{
         if(e.keyCode === 67){
             playSound(sounds[8].key)
         }
-    }
+    }, [])
     const playSound = (key) => {
         
         const audio = document.getElementById(key)
@@ -90,7 +90,7 @@ const DrumMachine = () =>{
 	};
     useEffect(() => {
         document.addEventListener("keydown", handleKeyDown)
-    }, [])
+    }, [handleKeyDown])
     return(
         <div id="drum-machine">
             <h1>Drumkit</h1>
--- src/index.js
+++ src/index.js
@@ -2,11 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 //import RandomQuoteGenerator from './randomQuoteGenerator'
-
 //import MarkdownPreviewer from './markdownPreviewer'
 import DrumMachine from './drumMachine'
-
-
 import reportWebVitals from './reportWebVitals';
 
 
